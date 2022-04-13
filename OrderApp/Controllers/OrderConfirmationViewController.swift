2024babe0237208941df9abda957902e580a990d
Brian Aguirre <aguirre.28@osu.edu>
2022-04-13 19:31:19 -0400

    stretch goal: add a progress bar to the order confirmation screen and update it to show order progress

--- OrderApp/Controllers/OrderConfirmationViewController.swift
+++ OrderApp/Controllers/OrderConfirmationViewController.swift
@@ -10,14 +10,18 @@ import UIKit
 // This class/view controller presents a confirmation message that the order has been submitted along with an estimated wait time
 class OrderConfirmationViewController: UIViewController {
     
-    let minutesToPrepare: Int
+    var minutesToPrepare: Int
     
+    @IBOutlet var progressBar: UIProgressView!
     @IBOutlet var confirmationLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         confirmationLabel.text = "Thank you for your order! Your wait time is approximately \(minutesToPrepare) minutes."
+        progressBar.progress = 0
+        
+        setProgressTimer()
     }
     
     // Initialize with a prep time value
@@ -28,5 +32,16 @@ class OrderConfirmationViewController: UIViewController {
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    // Create a timer to increase the progress bar progress according to the elapsed time and the estimated prep time
+    func setProgressTimer() {
+        Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { timer in
+            self.progressBar.progress += Float(1.0/(Double(self.minutesToPrepare*60)))
+            
+            if self.progressBar.progress == 1 {
+                timer.invalidate()
+            }
+        }
     }
 }
